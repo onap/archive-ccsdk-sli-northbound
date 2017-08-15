1027b0c07b488bb5f4542bd38501f9a99da5c2cf
Dan Timoney <dtimoney@att.com>
2017-08-15 15:18:54 -0400

    Port sli/northbound to Carbon
    Updated sli/northbound repo to compile against OpenDaylight Carbon SR1 release
    
    Change-Id: I6aec68cf436e11f5fd5b7094eb5616536bead44f
    Issue-ID: CCSDK-25
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- dmaap-listener/src/main/scripts/stop-dmaap-listener.sh
+++ dmaap-listener/src/main/scripts/stop-dmaap-listener.sh
@@ -4,8 +4,8 @@
 # ============LICENSE_START=======================================================
 # openECOMP : SDN-C
 # ================================================================================
-# Copyright (C) 2017 ONAP Intellectual Property. All rights
-# 						reserved.
+# Copyright (C) 2017 AT&T Intellectual Property. All rights
+# 			reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
