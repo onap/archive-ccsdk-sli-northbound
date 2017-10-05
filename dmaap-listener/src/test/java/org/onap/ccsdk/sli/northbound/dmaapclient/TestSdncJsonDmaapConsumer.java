905f1d2016e8bc4d02a801017a31b20c4eb2119e
surya-huawei <a.u.surya@huawei.com>
2017-10-05 15:20:33 +0530

    Add a test for SdncJsonDmaapConsumer
    *Add a unit test in sli/northbound module
    This Test checks whether an InvalidMessageException is
    thrown when null is passed to processMsg method
    
    Issue-Id: CCSDK-106
    Change-Id: I52327377f4582d8c24cef87f0360b977f2f216fc
    Signed-off-by: surya-huawei <a.u.surya@huawei.com>

---
+++ dmaap-listener/src/test/java/org/onap/ccsdk/sli/northbound/dmaapclient/TestSdncJsonDmaapConsumer.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2017. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+ * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+ * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+ * Vestibulum commodo. Ut rhoncus gravida arcu.
+ */
+
+package org.onap.ccsdk.sli.northbound.dmaapclient;
+
+import org.junit.Test;
+
+public class TestSdncJsonDmaapConsumer {
+    private static final String DMAAP_LISTENER_PROPERTIES = "dmaap-listener.properties";
+    private static final String DMAAP_LISTENER_PROPERTIES_DIR = "src/test/resources";
+
+    @Test(expected = InvalidMessageException.class)
+    public void testProcessMsg_shouldThrowException() throws Exception {
+        SdncFlatJsonDmaapConsumer consumer = new SdncFlatJsonDmaapConsumer();
+        consumer.processMsg(null);
+    }
+}
