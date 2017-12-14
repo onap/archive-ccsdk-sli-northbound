22a18f12b2338c15dc266289955efa94ba99c157
prakash.e <prakash.e@huawei.com>
2017-12-14 16:27:14 +0530

    Removal of useless parenthesis
    Change-Id: Ida52e0fab68d33ef446378024441ca3af0762f70
    Issue-ID: CCSDK-151
    Signed-off-by: prakash.e <prakash.e@huawei.com>

--- ueb-listener/src/main/java/org/onap/ccsdk/sli/northbound/uebclient/SdncBaseModel.java
+++ ueb-listener/src/main/java/org/onap/ccsdk/sli/northbound/uebclient/SdncBaseModel.java
@@ -233,10 +233,10 @@ public class SdncBaseModel {
 	}
 
 	public String getCustomizationUUID() {
-		return ("\"" + customizationUUID + "\"");
+		return "\"" + customizationUUID + "\"";
 	}
 	public String getCustomizationUUIDNoQuotes() {
-		return (customizationUUID);
+		return customizationUUID;
 	}
 	public void setCustomizationUUID(String customizationUUID) {
 		this.customizationUUID = customizationUUID;
